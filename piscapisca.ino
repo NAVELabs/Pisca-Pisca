21d79b957355056c01dbb7c9a71dade3b6cb5191
Lucas Soares <lsoares267@gmail.com>
2016-12-27 20:57:16 -0200

    resolvendo bug

--- piscapisca.ino
+++ piscapisca.ino
@@ -29,3 +29,74 @@ void MudaSequencia()
 	{
 	  posicaoSequencia += 1; 
 	}
+}
+
+void loop() 
+{
+   switch(posicaoSequencia)
+  {
+    case 1:
+    Sequencia(200);
+    break;
+    
+    case 2:
+    Sequencia(100);
+    break;
+
+    case 3:
+    SequenciaDois(50);
+    break;
+
+    case 4:
+    SequenciaDois(200);
+    break;
+
+    case 5:
+    SequenciaDois(500);
+    break;
+  }
+}
+
+void Sequencia(int delayTime)
+{
+  digitalWrite(ledA,HIGH);
+  digitalWrite(ledB,HIGH);
+  digitalWrite(ledC,HIGH);
+  digitalWrite(ledD,HIGH);
+  
+  delay(delayTime);
+
+  digitalWrite(ledA,LOW);
+  digitalWrite(ledB,LOW);
+  digitalWrite(ledC,LOW);
+  digitalWrite(ledD,LOW);
+
+  delay(delayTime);
+}
+
+void SequenciaDois(int SecondDelay)
+{
+  digitalWrite(ledA,HIGH);
+  delay(SecondDelay);
+
+  digitalWrite(ledB,HIGH);
+  delay(SecondDelay);
+
+  digitalWrite(ledC,HIGH);
+  delay(SecondDelay);
+
+  digitalWrite(ledD,HIGH);
+  delay(SecondDelay); 
+
+  digitalWrite(ledA,LOW);
+  delay(SecondDelay);
+  
+  digitalWrite(ledB,LOW);
+  delay(SecondDelay);
+  
+  digitalWrite(ledC,LOW);
+  delay(SecondDelay);
+  
+  digitalWrite(ledD,LOW);
+  delay(SecondDelay);
+}
